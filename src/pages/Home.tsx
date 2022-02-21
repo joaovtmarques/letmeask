3221623c376776a848efcf0c91c7e18dcffd8a73
joaovtmarques <jvsilvam@outlook.com>
2022-02-21 11:37:09 -0300

    Create authentication with google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,27 @@
-import illustrationImg from "../assets/images/illustration.svg";
+import { useHistory } from "react-router-dom";
+
+import { useAuth } from "../hooks/useAuth";
+
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
+import illustrationImg from "../assets/images/illustration.svg";
 
-import "../styles/auth.scss";
 import { Button } from "../components/Button";
 
+import "../styles/auth.scss";
+
 export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
+    history.push("/rooms/new");
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -19,7 +35,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
